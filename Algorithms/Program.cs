a9261607c221e8d84a23ec337d64bf54fba38fa7
Anton Turubarov <molekular_man@mail.ru>
2024-09-25 23:49:42 +0300

    Program file.

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
-int[] array = { -58, -78, -59, -75, -62, 26, -74, -53, 28, -20, -13, -8, 62, 66, 68 };
+int[] array = { -58, -78, -59, -75, -62, 26, -74, -53, 28, -20, -13, -8, 62, 66, 68, 55 };
 
 Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
 
